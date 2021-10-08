44d595e37a7bbe65e138a32a3ec35f7fc32384a0
Nikita VASILEV <vassilev.nikita@gmail.com>
2021-10-08 19:05:24 +0200

    fixed norm

--- srcs/client.c
+++ srcs/client.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 18:27:43 by nvasilev          #+#    #+#             */
-/*   Updated: 2021/10/08 18:32:55 by nvasilev         ###   ########.fr       */
+/*   Updated: 2021/10/08 19:05:07 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 int	main(int argc, char const *argv[])
 {
 	(void)argv;
-
 	if (argc != 1)
 	{
 		ft_putstr_fd("Error: Too many arguments\n", 2);
