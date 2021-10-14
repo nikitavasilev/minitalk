cb49252d102459d5630ed65272a4cdf2bdb71b4c
Nikita VASILEV <vassilev.nikita@gmail.com>
2021-10-14 18:02:41 +0200

    atoi + isdigit protos

--- includes/minitalk.h
+++ includes/minitalk.h
@@ -6,13 +6,14 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 15:44:56 by nvasilev          #+#    #+#             */
-/*   Updated: 2021/10/14 15:53:11 by nvasilev         ###   ########.fr       */
+/*   Updated: 2021/10/14 18:00:55 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 # define _XOPEN_SOURCE 700
+# define _DEFAULT_SOURCE
 
 # include <stdio.h>
 # include <signal.h>
@@ -26,5 +27,7 @@ int		ft_putchar_fd(int c, int fd);
 void	ft_putstr_fd(const char *str, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_error(const char *str);
+int		ft_atoi(const char *str);
+int		ft_isdigit(int c);
 
 #endif
