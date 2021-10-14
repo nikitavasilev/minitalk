932816a3df2b2cc0370114d28fac094412835b40
Nikita VASILEV <vassilev.nikita@gmail.com>
2021-10-14 15:30:14 +0200

    exit failure

--- srcs/client.c
+++ srcs/client.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 18:27:43 by nvasilev          #+#    #+#             */
-/*   Updated: 2021/10/08 19:05:07 by nvasilev         ###   ########.fr       */
+/*   Updated: 2021/10/13 14:30:48 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@ int	main(int argc, char const *argv[])
 	if (argc != 1)
 	{
 		ft_putstr_fd("Error: Too many arguments\n", 2);
-		exit(0);
+		exit(EXIT_FAILURE);
 	}
+	while (true)
+		pause();
 	return (0);
 }
