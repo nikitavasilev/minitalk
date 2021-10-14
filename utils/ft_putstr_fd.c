0c0eab9c97dbce5c0c8ba1ed77cf8cd8db811fb4
Nikita VASILEV <vassilev.nikita@gmail.com>
2021-10-14 15:58:20 +0200

    parameter to const

--- utils/ft_putstr_fd.c
+++ utils/ft_putstr_fd.c
@@ -6,13 +6,13 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 17:07:15 by nvasilev          #+#    #+#             */
-/*   Updated: 2021/10/08 17:24:08 by nvasilev         ###   ########.fr       */
+/*   Updated: 2021/10/14 15:54:45 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
-void	ft_putstr_fd(char *str, int fd)
+void	ft_putstr_fd(const char *str, int fd)
 {
 	while (*str)
 		ft_putchar_fd(*(str++), fd);
