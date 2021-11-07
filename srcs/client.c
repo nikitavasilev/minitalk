0da59f86140af537c2f7aee6b59bde918f058bf0
Nikita VASILEV <vassilev.nikita@gmail.com>
2021-11-07 18:11:16 +0100

    fix bits count + exit

--- srcs/client.c
+++ srcs/client.c
@@ -6,43 +6,55 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 18:27:43 by nvasilev          #+#    #+#             */
-/*   Updated: 2021/10/14 18:00:58 by nvasilev         ###   ########.fr       */
+/*   Updated: 2021/11/07 18:01:52 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
-static void	decimal_conversion(char ascii, int power, int pid)
+static int	g_nb_signals = 0;
+
+static void	send_sign(pid_t pid, int nb)
 {
-	if (power > 0)
-		decimal_conversion(ascii / 2, power - 1, pid);
-	if ((ascii % 2) == 1)
-	{
+	if (nb == 0)
 		if (kill(pid, SIGUSR1) == -1)
-			ft_error("\033[0;31mERROR:\033[0m Connection not established.");
-	}
-	else
-	{
+			ft_error(RED "ERROR: " RST_CLR "Connection not established.");
+	if (nb == 1)
 		if (kill(pid, SIGUSR2) == -1)
-			ft_error("\033[0;31mERROR:\033[0m Connection not established.");
+			ft_error(RED "ERROR: " RST_CLR "Connection not established.");
+	g_nb_signals++;
+}
+
+static void	cut_bits(pid_t pid, char to_send)
+{
+	char	c;
+	int		nb;
+
+	c = 0;
+	while (c < 8)
+	{
+		nb = (to_send >> c++) & 1;
+		usleep(800);
+		send_sign(pid, nb);
 	}
-	usleep(100);
+}
+
+static void	cut_str(pid_t pid, const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		cut_bits(pid, str[i++]);
+	cut_bits(pid, 0);
 }
 
 static void	signal_handler(int signo)
 {
 	(void)signo;
-	ft_putstr_fd("Signal received.\n", STDOUT_FILENO);
-}
-
-static int	send_message(int server_pid, const char *msg)
-{
-	size_t	i;
-
-	i = 0;
-	while (msg[i])
-		decimal_conversion(msg[i++], 7, server_pid);
-	return (0);
+	ft_putnbr_fd(g_nb_signals, STDOUT_FILENO);
+	ft_putstr_fd(" bits received.\n", STDOUT_FILENO);
+	exit(EXIT_SUCCESS);
 }
 
 int	main(int argc, char const *argv[])
@@ -52,18 +64,18 @@ int	main(int argc, char const *argv[])
 	action.sa_handler = signal_handler;
 	sigemptyset(&action.sa_mask);
 	if (sigaction(SIGUSR2, &action, NULL) == -1)
-		ft_error("ERROR: Unable to create handler for SIGUSR2.");
+		ft_error(RED "ERROR: " RST_CLR "Unable to create handler for SIGUSR2.");
 	if (argc == 3)
-		send_message(ft_atoi(argv[1]), argv[2]);
+		cut_str(ft_atoi(argv[1]), argv[2]);
 	else if (argc < 3)
 	{
-		ft_putstr_fd("ERROR: Too few arguments.\n", STDOUT_FILENO);
-		ft_error("\nUsage:\n$ ./client [Servers's PID] [Message]");
+		ft_putstr_fd(RED"ERROR: "RST_CLR"Too few arguments.\n", STDOUT_FILENO);
+		ft_error(YEL "\nUsage:\n$ ./client [Servers's PID] [Message]" RST_CLR);
 	}
 	else
 	{
-		ft_putstr_fd("ERROR: Too many arguments.\n", STDOUT_FILENO);
-		ft_error("\nUsage:\n$ ./client [Servers's PID] [Message]");
+		ft_putstr_fd(RED"ERROR: "RST_CLR"Too many arguments.\n", STDOUT_FILENO);
+		ft_error(YEL "\nUsage:\n$ ./client [Servers's PID] [Message]" RST_CLR);
 	}
 	while (true)
 		pause();
