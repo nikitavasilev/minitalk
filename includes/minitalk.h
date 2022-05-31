bb7f9e09c568d76991be72dbdc1cd5ed8f66e217
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-05-31 07:57:49 +0200

    del lib includes + def posix_c_source for sigaction

--- includes/minitalk.h
+++ includes/minitalk.h
@@ -6,26 +6,21 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 15:44:56 by nvasilev          #+#    #+#             */
-/*   Updated: 2021/12/12 18:46:53 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/05/30 05:04:38 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
-# define _XOPEN_SOURCE 700
-# define _DEFAULT_SOURCE
+# ifndef _POSIX_C_SOURCE
+#  define _POSIX_C_SOURCE 199309L
+# endif
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 4096
 # endif
 
-# include <stdio.h>
-# include <signal.h>
-# include <stdlib.h>
-# include <unistd.h>
 # include <sys/types.h>
-# include <stdbool.h>
-# include "ansi_colors.h"
 
 // CLIENT
 void	check_args(int argc, char const *argv[]);
@@ -33,7 +28,7 @@ void	cut_str(pid_t pid, const char *str);
 
 // UTILS FUNCTIONS
 int		ft_putchar_fd(int c, int fd);
-void	ft_putstr_fd(const char *str, int fd);
+int		ft_putstr_fd(const char *str, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_error(const char *str);
 int		ft_atoi(const char *str);
